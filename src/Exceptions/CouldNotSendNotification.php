0e85f7bee8397a92a89b772e9cc6c4c5c564509a
Swapnil Bhavsar <swapnil@chitranu.com>
2020-09-27 22:19:01 +0530

    ✨ Add Textlocal specific exception messages

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,10 +2,23 @@
 
 namespace NotificationChannels\Textlocal\Exceptions;
 
+use NotificationChannels\Textlocal\TextlocalMessage;
+
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($response)
+    public static function invalidReceiver()
     {
-        return new static("Descriptive error message.");
+        return new static(
+            'The notifiable did not have a receiving phone number. Add a routeNotificationForTextlocal method or one of the conventional attributes to your notifiable.'
+        );
+    }
+
+    public static function invalidMessageObject($message)
+    {
+        $type = is_object($message) ? get_class($message) : gettype($message);
+
+        return new static(
+            'Notification was not sent. The message should be a instance of `'.TextlocalMessage::class."` and a `{$type}` was given."
+        );
     }
 }
